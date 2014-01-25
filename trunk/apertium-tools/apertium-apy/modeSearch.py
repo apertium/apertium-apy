5b7cd5a37feac96a8e3ce7cce0929c88e1d2365e
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-01-25 01:50:06 +0000

    Optimize mode search performance (hopefully)

--- trunk/apertium-tools/apertium-apy/modeSearch.py
+++ trunk/apertium-tools/apertium-apy/modeSearch.py
@@ -16,12 +16,12 @@ def searchPath(path):
                     l1, l2 = modes['pair'][0].sub('\g<1>', filename), modes['pair'][0].sub('\g<2>', filename)
                     pairTuple = (dirpath, toAlpha3Code(l1), toAlpha3Code(l2))
                     modes['pair'][1].append(pairTuple)
-
-        for filename in filenames:
-            for mode, info in modes.items():
-                if mode != 'pair' and info[0].match(filename):
-                    mode = info[0].sub('\g<1>', filename) #e.g. en-es-anmorph
-                    lang = '-'.join(map(toAlpha3Code, info[0].sub('\g<2>', filename).split('-'))) #e.g. en-es
-                    info[1].append((dirpath, mode, lang))
+        elif dirpath.endswith('modes'):
+            for filename in filenames:
+                for mode, info in modes.items():
+                    if mode != 'pair' and info[0].match(filename):
+                        mode = info[0].sub('\g<1>', filename) #e.g. en-es-anmorph
+                        lang = '-'.join(map(toAlpha3Code, info[0].sub('\g<2>', filename).split('-'))) #e.g. en-es
+                        info[1].append((dirpath, mode, lang))
     
     return {mode: result[1] for mode, result in modes.items()}
