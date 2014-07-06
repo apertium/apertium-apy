ce9778df99b7970ddc6db28b895343fa1d2fb114
xavivars <xavivars@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-07-06 13:34:06 +0000

    [Softcatalà] Integrate ScaleMT-like logs into APY

--- trunk/apertium-tools/apertium-apy/util.py
+++ trunk/apertium-tools/apertium-apy/util.py
@@ -3,6 +3,7 @@
 
 import sqlite3, re, os, logging
 from subprocess import Popen, PIPE
+from datetime import datetime
 
 iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"}
 '''
@@ -175,3 +176,30 @@ def processPerWord(analyzers, taggers, lang, modes, query):
             return
 
     return (outputs, tagger_lexicalUnits, morph_lexicalUnits)
+      
+def getTimestamp():
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+
+def timedeltaToMilliseconds(td):
+    return td.days*86400000 + td.seconds*1000 + int(td.microseconds/1000)
+
+def scaleMtLog(status, time, tInfo, key, length):
+	logging.getLogger('scale-mt').error("%s %s %s html %s %s %s %s %s %s", 
+									  getTimestamp(), 
+									  timedeltaToMilliseconds(time), 
+									  tInfo.langpair, 
+									  key,
+									  tInfo.ip,
+									  tInfo.referer,
+									  status,
+									  length,
+									  'null'
+	)
+
+
+class TranslationInfo:
+    def __init__(self, handler):
+        self.langpair = handler.get_argument('langpair')
+        self.key = handler.get_argument('key', default = 'null')
+        self.ip = handler.request.remote_ip
+        self.referer = handler.request.headers.get('Referer', 'null')
