7ba1efb0f3d6644635715428f87ccd37dc758061
Xavi Ivars <xavi.ivars@gmail.com>
2014-10-15 22:37:51 +0000

    [APY] New mode -M to keep unknown words in memory until a threshold is achieved (missing file)

--- util.py
+++ util.py
@@ -4,6 +4,8 @@
 import sqlite3, re, os, logging
 from subprocess import Popen, PIPE
 from datetime import datetime
+import threading
+from collections import defaultdict
 
 iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"}
 '''
@@ -76,6 +78,59 @@ def noteUnknownToken(token, pair, dbPath):
     c.execute('CREATE TABLE IF NOT EXISTS missingFreqs (pair TEXT, token TEXT, frequency INTEGER, UNIQUE(pair, token))')
     c.execute('INSERT OR REPLACE INTO missingFreqs VALUES (:pair, :token, COALESCE((SELECT frequency FROM missingFreqs WHERE pair=:pair AND token=:token), 0) + 1)', {'pair': pair, 'token': token})
     missingFreqsDBConn.commit()
+
+
+unknownLock = threading.RLock()
+unknownWords = defaultdict(lambda: defaultdict(lambda: 0))
+unknownCount = 0
+
+def inMemoryUnknownToken(token, pair, dbPath, limit):
+    global unknownLock
+    global unknownCount
+    global unknownWords
+
+    try:
+        unknownLock.acquire()
+        unknownWords[pair][token] += 1
+        unknownCount += 1
+
+        if unknownCount > limit:
+            flushUnknownWords(dbPath)
+            unknownWords.clear()
+            unknownCount = 0
+    finally:
+        unknownLock.release()
+
+
+def flushUnknownWords(dbPath):
+    global unknownWords
+    global missingFreqsDBConn
+
+    timeBefore = datetime.now()
+
+    if not missingFreqsDBConn:
+        missingFreqsDBConn = sqlite3.connect(dbPath)
+
+    c = missingFreqsDBConn.cursor()
+    c.execute("PRAGMA synchronous = NORMAL")
+
+    c.execute('CREATE TABLE IF NOT EXISTS missingFreqs (pair TEXT, token TEXT, frequency INTEGER, UNIQUE(pair, token))')
+
+    c.executemany('INSERT OR REPLACE INTO missingFreqs VALUES (:pair, :token, COALESCE((SELECT frequency FROM missingFreqs WHERE pair=:pair AND token=:token), 0) + :amount)',
+                  ({'pair': pair, 'token': token, 'amount' : unknownWords[pair][token]} for pair in unknownWords for token in unknownWords[pair]))
+
+    missingFreqsDBConn.commit()
+
+    ms = timedeltaToMilliseconds(datetime.now() - timeBefore)
+    logging.info("\tSaving %s unknown words to the DB (%s ms)", unknownCount, ms)
+
+def closeDb():
+    global missingFreqsDBConn
+    if not missingFreqsDBConn:
+        logging.warning('no connection')
+        return
+    logging.warning('closing connection')
+    missingFreqsDBConn.close()
 
 def apertium(input, dir, mode, formatting=None):
     p1 = Popen(['echo', input], stdout=PIPE)
