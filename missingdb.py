48b0af00c2160fc314510e3eb8ec8937e790462f
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-04-29 14:47:16 +0200

    missingdb: close cursor each time as well

--- missingdb.py
+++ missingdb.py
@@ -5,6 +5,7 @@ import sqlite3, logging
 from datetime import datetime
 import threading
 from collections import defaultdict
+from contextlib import closing
 
 class MissingDb(object):
     def __init__(self, dbPath, wordmemlimit):
@@ -30,15 +31,15 @@ class MissingDb(object):
             if not self.conn:
                 self.conn = sqlite3.connect(self.dbPath)
 
-            c = self.conn.cursor()
-            c.execute("PRAGMA synchronous = NORMAL")
-            c.execute('CREATE TABLE IF NOT EXISTS missingFreqs (pair TEXT, token TEXT, frequency INTEGER, UNIQUE(pair, token))')
-            c.executemany('INSERT OR REPLACE INTO missingFreqs VALUES (:pair, :token, COALESCE((SELECT frequency FROM missingFreqs WHERE pair=:pair AND token=:token), 0) + :amount)',
-                          ({'pair': pair,
-                            'token': token,
-                            'amount' : self.words[pair][token]}
-                           for pair in self.words
-                           for token in self.words[pair]))
+            with closing(self.conn.cursor()) as c:
+                c.execute("PRAGMA synchronous = NORMAL")
+                c.execute('CREATE TABLE IF NOT EXISTS missingFreqs (pair TEXT, token TEXT, frequency INTEGER, UNIQUE(pair, token))')
+                c.executemany('INSERT OR REPLACE INTO missingFreqs VALUES (:pair, :token, COALESCE((SELECT frequency FROM missingFreqs WHERE pair=:pair AND token=:token), 0) + :amount)',
+                            ({'pair': pair,
+                                'token': token,
+                                'amount' : self.words[pair][token]}
+                            for pair in self.words
+                            for token in self.words[pair]))
             self.conn.commit()
         ms = timedeltaToMilliseconds(datetime.now() - timeBefore)
         logging.info("\tSaving %s unknown words to the DB (%s ms)", self.wordcount, ms)
@@ -48,6 +49,7 @@ class MissingDb(object):
             logging.warning('no connection on closeDb')
             return
         logging.warning('closing connection')
+        self.conn.commit()
         self.conn.close()
         self.conn = None
 
