57ffb18f3d6235bea0d8d0baefe20a7a38478b30
mono <monish1247@gmail.com>
2017-07-12 23:47:24 +0530

    Remove blank line

--- servlet.py
+++ servlet.py
@@ -1264,6 +1264,7 @@ def setupHandler(
     Handler.scaleMtLogs = scaleMtLogs
     Handler.verbosity = verbosity
     Handler.doc_pipe_sem = Semaphore(max_doc_pipes)
+
     modes = searchPath(pairs_path, verbosity=verbosity)
     if nonpairs_path:
         src_modes = searchPath(nonpairs_path, include_pairs=False, verbosity=verbosity)
