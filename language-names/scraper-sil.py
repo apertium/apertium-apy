98bfa9404f4917dbf8ea8793bf99f8c884584630
Sushain Cherivirala <sushain@skc.name>
2017-12-31 01:25:20 -0600

    Add SIL scraped language names

---
+++ language-names/scraper-sil.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import argparse
+import urllib.request
+import re
+import sys
+
+sil_names = 'http://www-01.sil.org/iso639-3/iso-639-3.tab'
+tsv_format = re.compile(r'(?P<Id>.*?)\s(?P<Part2B>.*?)\s(?P<Part2T>.*?)\s(?P<Part1>.*?)\s(?P<Scope>.*?)\s(?P<Language_Type>.*?)\s(?P<Ref_Name>.*?)\s(?P<Comment>.*?)')  # noqa: E501
+insert_template = '''INSERT OR IGNORE INTO "%s" VALUES(NULL,"%s","%s","%s");'''
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Scrape SIL for language names in English.')
+    parser.add_argument('-t', '--table', help='language names table', default='languageNames')
+    args = parser.parse_args()
+
+    print('BEGIN TRANSACTION;')
+
+    sil_tsv = urllib.request.urlopen(sil_names).read().decode('utf-8')  # Python's csv module chokes badly on this
+    tsv_contents = sil_tsv.splitlines()
+    for tsv_line in tsv_contents[1:]:  # skip the header
+        matches = tsv_format.match(tsv_line)
+
+        iso639_3 = matches.group('Id')
+        iso639_1 = matches.group('Part1')
+        iso639 = iso639_1 or iso639_3
+
+        name = matches.group('Ref_Name')
+
+        if iso639 and name:
+            print(insert_template % (args.table, 'en', iso639, name))
+        else:
+            sys.stdout.write('!!! Unable to parse %s !!!\n' % repr(tsv_line))
+
+    print('COMMIT;')
