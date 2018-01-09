ae5e6fcd38bf1fa15b5db2073edde494e509400f
Jonathan Washington <jonathan.north.washington@gmail.com>
2018-01-08 22:19:56 -0500

    native names for khk and kmr

--- language-names/manual.sql
+++ language-names/manual.sql
@@ -333,6 +333,10 @@ INSERT INTO "additions" VALUES(NULL,'en','qve','Quechua');
 INSERT INTO "additions" VALUES(NULL,'en','ssp','Spanish Sign Language');
 INSERT INTO "additions" VALUES(NULL,'en','khk','Khalkha Mongolian');
 
+INSERT INTO "additions" VALUES(NULL,'khk','khk','монгол хэл');
+INSERT INTO "additions" VALUES(NULL,'kmr','kmr','Kurmancî');
+
+
 INSERT INTO "languageNames" (lg, inLg, name) select lg, inLg, name from "fixes";
 INSERT INTO "languageNames" (lg, inLg, name) select lg, inLg, name from "additions";
 COMMIT;
