a8551680cb58f667e693b22b0d6caca5bf6dcb6a
Sushain Cherivirala <sushain@skc.name>
2018-03-24 08:21:36 -0500

    No more symlinks

--- servlet.py
+++ servlet.py
@@ -1,1 +1,10 @@
-apertium_apy/apy.py
+#!/usr/bin/env python3
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), './apertium_apy'))
+from apy import main  # noqa: E402
+
+if __name__ == '__main__':
+    main()
