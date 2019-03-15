f480c6e833650edb2bed1cb26b9b616a59538214
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-03-16 02:57:24 +0530

    Added headers to tsv files and using a function to insert values to the database

--- language_names/manual.py
+++ language_names/manual.py
@@ -3,27 +3,33 @@ import textwrap
 import csv
 
 
+def insert_values(c, filename):
+    c.execute(textwrap.dedent("""
+        CREATE TABLE IF NOT EXISTS {file} (
+            id INTEGER PRIMARY KEY,
+            lg TEXT,
+            inLg TEXT,
+            name TEXT,
+            UNIQUE(lg, inLg) ON CONFLICT REPLACE);
+        """.format(file=filename)))
+    with open(filename + '.tsv', 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        next(reader)
+        for row in reader:
+            c.execute('INSERT INTO ' + filename + ' VALUES (?, ?, ?, ?)', (None, row[0], row[1], row[2]))
+
+
 def populate_database():
-    conn = sqlite3.connect('test.db')
+    conn = sqlite3.connect('langNames.db')
     c = conn.cursor()
-    c.execute(textwrap.dedent("""
-        PRAGMA foreign_keys=OFF;"""))
-    c.execute(textwrap.dedent("""
-        BEGIN TRANSACTION;"""))
-    c.execute(textwrap.dedent("""
-        CREATE TABLE IF NOT EXISTS fixes (id integer primary key, lg text, inLg text, name text, unique(lg, inLg) on conflict replace);
-        """))
-    with open('fixes.tsv', 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-        for row in reader:
-            c.execute('INSERT INTO fixes VALUES (?, ?, ?, ?)', (None, row[0], row[1], row[2]))
-    c.execute(textwrap.dedent("""
-        CREATE TABLE IF NOT EXISTS additions (id integer primary key, lg text, inLg text, name text, unique(lg, inLg) on conflict replace);
-        """))
-    with open('additions.tsv', 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-        for row in reader:
-            c.execute('INSERT INTO additions VALUES (?, ?, ?, ?)', (None, row[0], row[1], row[2]))
+    c.execute("""
+        PRAGMA foreign_keys=OFF;
+        """)
+    c.execute("""
+        BEGIN TRANSACTION;
+        """)
+    insert_values(c, 'fixes')
+    insert_values(c, 'additions')
     conn.commit()
     c.close()
 
