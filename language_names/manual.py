ec4e14331a045ab875138618390cf006aa66436b
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-03-17 18:23:06 +0530

    Added tsv sources for langNames.db to be converted at build time by manual.py

---
+++ language_names/manual.py
@@ -0,0 +1,30 @@
+import sqlite3
+import textwrap
+import csv
+
+def insert_values(c, filename):
+    c.execute(textwrap.dedent(f"""
+        CREATE TABLE IF NOT EXISTS {filename} (
+            id INTEGER PRIMARY KEY,
+            lg TEXT,
+            inLg TEXT,
+            name TEXT,
+            UNIQUE(lg, inLg) ON CONFLICT REPLACE);
+        """))
+    with open('{}.tsv'.format(filename),'r') as f:
+        reader = csv.DictReader(f, delimiter='\t')
+        for row in reader:
+            c.execute('INSERT INTO {} VALUES (?, ?, ?, ?)'.format(filename), (None, row['lg'], row['inLg'], row['name']))
+
+def populate_database():
+    conn = sqlite3.connect('langNames.db')
+    c = conn.cursor()
+    c.execute("""PRAGMA foreign_keys=OFF;""")
+    c.execute("""BEGIN TRANSACTION;""")
+    insert_values(c, 'fixes')
+    insert_values(c, 'additions')
+    conn.commit()
+    c.close()
+
+if __name__ == '__main__':
+    populate_database()
