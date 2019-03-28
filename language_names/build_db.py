7a6a116c778f37591b2b9e4ef0b5d41085998e76
Sushain Cherivirala <sushain@skc.name>
2019-03-27 19:03:32 -0500

    Clean up build-db script and add some logging

--- language_names/build_db.py
+++ language_names/build_db.py
@@ -1,34 +1,57 @@
+#!/usr/bin/env python3
+
+import argparse
 import csv
+import logging
 import sqlite3
-import sys
 import textwrap
 
+logging.basicConfig(level=logging.INFO)
 
-def insert_values(c, filename):
+
+def count_language_names(c, table):
+    return c.execute('SELECT COUNT(*) FROM %s' % table).fetchone()[0]
+
+
+def populate_database(args):
+    conn = sqlite3.connect(args.db)
+    c = conn.cursor()
+
+    # Set up the database.
+    c.execute('PRAGMA foreign_keys=OFF')
     c.execute(textwrap.dedent("""
-        CREATE TABLE IF NOT EXISTS languageNames (
+        CREATE TABLE IF NOT EXISTS %s (
             id INTEGER PRIMARY KEY,
             lg TEXT,
             inLg TEXT,
             name TEXT,
-            UNIQUE(lg, inLg) ON CONFLICT REPLACE);
-        """))
-    with open(filename, 'r') as f:
-        reader = csv.DictReader(f, delimiter='\t')
-        for row in reader:
-            c.execute('INSERT INTO languageNames VALUES (?, ?, ?, ?)', (None, row['lg'], row['inLg'], row['name']))
-
-
-def populate_database():
-    conn = sqlite3.connect(sys.argv[1])
-    c = conn.cursor()
-    c.execute('PRAGMA foreign_keys=OFF;')
-    c.execute('BEGIN TRANSACTION;')
-    for filename in sys.argv[2:]:
-        insert_values(c, filename)
+            UNIQUE(lg, inLg) ON CONFLICT REPLACE)
+        """ % args.table))
     conn.commit()
+
+    # Add names from each TSV.
+    for filename in args.files:
+        initial_count = count_language_names(c, args.table)
+        c.execute('BEGIN TRANSACTION')
+        with open(filename) as f:
+            rows = list(csv.DictReader(f, delimiter='\t'))
+            insertion_count = len(rows)
+            for row in rows:
+                c.execute('INSERT INTO %s VALUES (?, ?, ?, ?)' % args.table,
+                          (None, row['lg'], row['inLg'], row['name']))
+        conn.commit()
+        new_insertions_count = count_language_names(c, args.table) - initial_count
+        logging.info('Inserted %d names from %s (%d new, %d updates)',
+                     insertion_count, filename, new_insertions_count,
+                     insertion_count - new_insertions_count)
+
     c.close()
 
 
 if __name__ == '__main__':
-    populate_database()
+    parser = argparse.ArgumentParser(description='Build language names database from TSVs.')
+    parser.add_argument('db', help='Language names database', default='langNames.db')
+    parser.add_argument('files', nargs='+', help='TSV files with language names')
+    parser.add_argument('--table', help='language names table', default='languageNames')
+    args = parser.parse_args()
+    populate_database(args)
