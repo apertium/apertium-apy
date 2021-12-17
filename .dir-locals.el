81e22fcdac297cd6045755112788566d61db03fc
Vaibhav Mishra <95900540+Vaibhavmishra78@users.noreply.github.com>
2021-12-17 12:01:34 +0530

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((flycheck-flake8-maximum-line-length . 180)))
- (python-mode . ((flycheck-python-mypy-args . ("run" "mypy" "--check-untyped-defs" "--python-version" "3.8"))
-                 (flycheck-python-mypy-executable . "pipenv"))))
+((nil .((flycheck-flake8-maximum-line-length .180)))
+ (python-mode .((flycheck-python-mypy-args .("run" "mypy" "--check-untyped-defs" "--python-version" "3.8"))
+                 (flycheck-python-mypy-executable ."pipenv"))))
