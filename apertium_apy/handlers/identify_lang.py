809b635460d9a077ee351981c1c3ac702f506d92
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2023-01-06 15:53:49 +0100

    Option to use fasttext for language identification

--- apertium_apy/handlers/identify_lang.py
+++ apertium_apy/handlers/identify_lang.py
@@ -3,6 +3,10 @@ from datetime import timedelta
 from tornado import gen
 
 try:
+    import fasttext
+except ImportError:
+    fasttext = None
+try:
     import cld2full as cld2  # type: ignore
 except ImportError:
     cld2 = None
@@ -11,20 +15,46 @@ from apertium_apy.handlers.base import BaseHandler
 from apertium_apy.utils import get_coverages, to_alpha3_code
 
 
+def fasttext_strip_prefix(s):
+    """Remove the initial __label__ prefix"""
+    return s[9:]
+
+
+def fasttext_identify(model, text):
+    # grab a bunch since currently the model might predict stuff outside possible_langs – it's still fast:
+    results = model.predict(text, k=200, threshold=0.001)
+    if results[0]:
+        possible_langs = zip(map(fasttext_strip_prefix, results[0]),
+                             results[1])
+        return {to_alpha3_code(possible_lang[0]): possible_lang[1]
+                for possible_lang in possible_langs}
+    else:
+        return {'nob': 1.0}  # TODO: better default
+
+
+def cld_identify(text):
+    cld_results = cld2.detect(text)
+    if cld_results[0]:
+        possible_langs = filter(lambda x: x[1] != 'un', cld_results[2])
+        return {to_alpha3_code(possible_lang[1]): possible_lang[2]
+                for possible_lang in possible_langs}
+    else:
+        return {'nob': 1.0}  # TODO: better default
+
+
 class IdentifyLangHandler(BaseHandler):
+    fasttext = None
+
     @gen.coroutine
     def get(self):
         text = self.get_argument('q')
         if not text:
             return self.send_error(400, explanation='Missing q argument')
 
-        if cld2:
-            cld_results = cld2.detect(text)
-            if cld_results[0]:
-                possible_langs = filter(lambda x: x[1] != 'un', cld_results[2])
-                self.send_response({to_alpha3_code(possible_lang[1]): possible_lang[2] for possible_lang in possible_langs})
-            else:
-                self.send_response({'nob': 100})  # TODO: Some more reasonable response
+        if self.fasttext is not None:
+            self.send_response(fasttext_identify(self.fasttext, text))
+        elif cld2:
+            self.send_response(cld_identify(text))
         else:
             try:
                 coverages = yield gen.with_timeout(
