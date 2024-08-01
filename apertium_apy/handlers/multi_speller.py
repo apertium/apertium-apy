637307e34caef0d456965c0e2a6d79d73f74b924
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-08-01 22:25:13 +0530

    fix suggestions not showing up

--- apertium_apy/handlers/multi_speller.py
+++ apertium_apy/handlers/multi_speller.py
@@ -2,7 +2,7 @@ import logging
 import os
 from tornado import gen
 from apertium_apy.handlers.base import BaseHandler
-from apertium_apy.utils import to_alpha2_code
+from apertium_apy.utils import to_alpha3_code
 from apertium_apy.utils.translation import translate_simple
 
 class MultiSpellerHandler(BaseHandler):
@@ -16,15 +16,25 @@ class MultiSpellerHandler(BaseHandler):
             self.send_error(400, explanation="Invalid spell checker specified. Only 'voikko' and 'divvun' are allowed.")
             return
         
+        if '-' in in_mode:
+            l1, l2 = map(to_alpha3_code, in_mode.split('-', 1))
+            in_mode = '%s-%s' % (l1, l2)
         in_mode = self.find_fallback_mode(in_mode, self.spellers)
+        logging.info(in_text)
+        logging.info(self.get_argument('lang'))
+        logging.info(in_mode)
+        logging.info(self.spellers)
         
         if in_mode in self.spellers:
-            mode = to_alpha2_code(in_mode)
             tokens = in_text.split()
+            logging.info(self.spellers[in_mode])
+            [base_path, mode] = self.spellers[in_mode]
+
+            path = self.find_speller_path(base_path, in_mode)
+
             units = []
-            
             for token in tokens:
-                result = yield self.check_spelling(token, mode, spell_checker)
+                result = yield self.check_spelling(token, mode, base_path, path, spell_checker)
                 units.append(self.parse_result(token, result, spell_checker))
             
             self.send_response(units)
@@ -32,14 +42,20 @@ class MultiSpellerHandler(BaseHandler):
             error_explanation = f"Error 404: Spelling mode for {in_mode} is not installed"
             self.send_error(404, explanation=error_explanation)
     
+    def find_speller_path(self, base_path, in_mode):
+        for root, _, files in os.walk(base_path):
+            for file in files:
+                if file == f"{in_mode}.zhfst":
+                    return os.path.join(root, file)
+        return base_path
+
     @gen.coroutine
-    def check_spelling(self, token, mode, spell_checker):
+    def check_spelling(self, token, mode, base_path, path, spell_checker):
         if spell_checker == 'voikko':
-            commands = [['voikkospell', '-d', mode, '-s']]
+            formatting = 'none'
+            commands = [['apertium', '-d', base_path, '-f', formatting, mode]]
         elif spell_checker == 'divvun':
-            base_path = os.path.expanduser("~")
-            speller_path = os.path.join(base_path, ".voikko", "3", f'{mode}.zhfst')
-            commands = [['divvunspell', 'suggest', '-a', speller_path]]
+            commands = [['divvunspell', 'suggest', '-a', path]]
 
         result = yield translate_simple(token, commands)
         return result
@@ -54,16 +70,23 @@ class MultiSpellerHandler(BaseHandler):
         known = False
         suggestions = []
         lines = result.strip().split('\n')
+        
         for line in lines:
             line = line.strip()
-            if line.startswith('C: '):
+            if f'"{token}" is in the lexicon' in line:
                 known = True
+                break
+            elif f'"{token}" is NOT in the lexicon' in line:
+                known = False
+            elif line.startswith('Corrections for'):
+                continue
+            elif line.startswith('Unable to correct'):
                 suggestions = []
                 break
-            elif line.startswith('W: '):
-                known = False
-            elif line.startswith('S: '):
-                suggestions.append(line[3:].strip())
+            elif line:
+                suggestion = line.split()[0].strip()
+                suggestions.append(suggestion)
+        
         return {'token': token, 'known': known, 'sugg': suggestions}
     
     def parse_divvun_result(self, token, result):
