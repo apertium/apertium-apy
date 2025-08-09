bf6fb7dedcb9d0b0ef34e9049afd4899e706c32a
vykliuk <alinavykliuk@gmail.com>
2025-08-09 03:31:44 -0400

    support extra tags

--- apertium_apy/handlers/billookup.py
+++ apertium_apy/handlers/billookup.py
@@ -58,34 +58,50 @@ class BillookupHandler(BaseHandler):
                 word = form.split("<", 1)[0]
                 tags = re.findall(r"<([^>]+)>", form)
                 if not tags:
-                    return word
+                    return word, []
                 pos = tags[0]
                 subcats = allowed.get(pos, [])
                 filtered = []
+                extra = []
                 for t in tags[1:]:
-                    if t in subcats and t not in filtered:
-                        filtered.append(t)
+                    if t in subcats:
+                        if t not in filtered:
+                            filtered.append(t)
+                    else:
+                        tag_with_brackets = f"<{t}>"
+                        if tag_with_brackets not in extra:
+                            extra.append(tag_with_brackets)
                 tag_str = f"<{pos}>" + "".join(f"<{t}>" for t in filtered)
-                return f"{word}{tag_str}"
+                return f"{word}{tag_str}", extra
 
             consolidated = {}
             for item in raw_results:
                 for src in item:
-                    norm_src = normalize(src)
+                    norm_src, extra_src = normalize(src)
                     if norm_src not in consolidated:
-                        consolidated[norm_src] = []
+                        consolidated[norm_src] = {"targets": {}, "extra_tags": []}
+                    for tag in extra_src:
+                        if tag not in consolidated[norm_src]["extra_tags"]:
+                            consolidated[norm_src]["extra_tags"].append(tag)
                     for tgt in item[src]:
-                        norm_tgt = normalize(tgt)
-                        if norm_tgt not in consolidated[norm_src]:
-                            consolidated[norm_src].append(norm_tgt)
+                        norm_tgt, _ = normalize(tgt)
+                        if norm_tgt not in consolidated[norm_src]["targets"]:
+                            consolidated[norm_src]["targets"][norm_tgt] = True
 
             results = []
-            for src, tgts in consolidated.items():
-                tgt_list = []
-                for t in tgts:
-                    tgt_list.append(t)
-                entry = {}
-                entry[src] = tgt_list
+            for src, data in consolidated.items():
+                tgt_list = list(data["targets"].keys())
+                if data["extra_tags"]:
+                    extra_combined = "".join(data["extra_tags"])
+                    entry = {
+                        src: tgt_list,
+                        "extra-tags": [extra_combined]
+                    }
+                else:
+                    entry = {
+                        src: tgt_list,
+                        "extra-tags": []
+                    }
                 results.append(entry)
 
             self.send_response({
@@ -102,6 +118,6 @@ class BillookupHandler(BaseHandler):
     @gen.coroutine
     def get(self):
         pair = self.get_pair_or_error(self.get_argument('langpair'))
-        
+
         if pair is not None:
             yield self.lookup_and_respond(pair, self.get_argument('q'))
