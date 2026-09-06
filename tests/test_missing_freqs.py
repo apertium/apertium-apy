apertium/apertium-apy

commit 34d2cdae8fa063d8260540381041136b8a9449e5

Reorganize everything

apertium_apy/__init__.py
@@ -1,0 +1,12 @@
+import random
+import string
+
+if False:
+    from typing import Optional  # noqa: F401
+
+    import apertium_apy.missingdb  # noqa: F401
+
+missing_freqs_db = None  # type: Optional[missingdb.MissingDb]  # has to be global for sig_handler :-/
+
+RECAPTCHA_VERIFICATION_URL = 'https://www.google.com/recaptcha/api/siteverify'
+BYPASS_TOKEN = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(24))

apertium_apy/handlers/base.py
@@ -1,0 +1,181 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+import tornado
+from tornado import escape
+from tornado.escape import utf8
+from tornado.locks import Semaphore
+
+if False:
+    from typing import Dict, Optional, List, Tuple  # noqa: F401
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    pairs = {}  # type: Dict[str, str]
+    analyzers = {}  # type: Dict[str, Tuple[str, str]]
+    generators = {}  # type: Dict[str, Tuple[str, str]]
+    taggers = {}  # type: Dict[str, Tuple[str, str]]
+    spellers = {}  # type: Dict[str, Tuple[str, str]]
+    # (l1, l2): [translation.Pipeline], only contains flushing pairs!
+    pipelines = {}  # type: Dict[str, List]
+    pipelines_holding = []  # type: List
+    callback = None
+    timeout = None
+    scale_mt_logs = False
+    verbosity = 0
+
+    # dict representing a graph of translation pairs; keys are source languages
+    # e.g. pairs_graph['eng'] = ['fra', 'spa']
+    pairs_graph = {}  # type: Dict[str, List[str]]
+    # 2-D dict storing the shortest path for a chained translation pair
+    # keys are source and target languages
+    # e.g. paths['eng']['fra'] = ['eng', 'spa', 'fra']
+    paths = {}  # type: Dict[str, Dict[str, List[str]]]
+
+    stats = {
+        'startdate': datetime.now(),
+        'useCount': {},
+        'vmsize': 0,
+        'timing': [],
+    }
+
+    # (l1, l2): translation.ParsedModes
+    pipeline_cmds = {}  # type: Dict
+    max_pipes_per_pair = 1
+    min_pipes_per_pair = 0
+    max_users_per_pipe = 5
+    max_idle_secs = 0
+    restart_pipe_after = 1000
+    doc_pipe_sem = Semaphore(3)
+    # Empty the url_cache[pair] when it's this full:
+    max_inmemory_url_cache = 1000  # type: int
+    url_cache = {}  # type: Dict[Tuple[str, str], Dict[str, str]]
+    url_cache_path = None  # type: Optional[str]
+    # Keep half a gig free when storing url_cache to disk:
+    min_free_space_disk_url_cache = 512 * 1024 * 1024  # type: int
+
+    def initialize(self):
+        self.callback = self.get_argument('callback', default=None)
+
+    @classmethod
+    def init_pairs_graph(cls):
+        for pair in cls.pairs:
+            lang1, lang2 = pair.split('-')
+            if lang1 in cls.pairs_graph:
+                cls.pairs_graph[lang1].append(lang2)
+            else:
+                cls.pairs_graph[lang1] = [lang2]
+
+    @classmethod
+    def calculate_paths(cls, start):
+        nodes = set()
+        for pair in map(lambda x: x.split('-'), cls.pairs):
+            nodes.add(pair[0])
+            nodes.add(pair[1])
+        dists = {}
+        prevs = {}
+        dists[start] = 0
+
+        while nodes:
+            u = min(nodes, key=lambda u: dists.get(u, sys.maxsize))
+            nodes.remove(u)
+            for v in cls.pairs_graph.get(u, []):
+                if v in nodes:
+                    other = dists.get(u, sys.maxsize) + 1   # TODO: weight(u, v) -- lower weight = better translation
+                    if other < dists.get(v, sys.maxsize):
+                        dists[v] = other
+                        prevs[v] = u
+
+        cls.paths[start] = {}
+        for u in prevs:
+            prev = prevs[u]
+            path = [u]
+            while prev:
+                path.append(prev)
+                prev = prevs.get(prev)
+            cls.paths[start][u] = list(reversed(path))
+
+    @classmethod
+    def init_paths(cls):
+        for lang in cls.pairs_graph:
+            cls.calculate_paths(lang)
+
+    def log_vmsize(self):
+        if self.verbosity < 1:
+            return
+        scale = {'kB': 1024, 'mB': 1048576,
+                 'KB': 1024, 'MB': 1048576}
+        try:
+            for line in open('/proc/%d/status' % os.getpid()):
+                if line.startswith('VmSize:'):
+                    _, num, unit = line.split()
+                    break
+            vmsize = int(num) * scale[unit]
+            if vmsize > self.stats['vmsize']:
+                logging.warning('VmSize of %s from %d to %d' % (os.getpid(), self.stats['vmsize'], vmsize))
+                self.stats['vmsize'] = vmsize
+        except Exception as e:
+            # Don't fail just because we couldn't log:
+            logging.info('Exception in log_vmsize: %s' % e)
+
+    def send_response(self, data):
+        self.log_vmsize()
+        if isinstance(data, dict) or isinstance(data, list):
+            data = escape.json_encode(data)
+            self.set_header('Content-Type', 'application/json; charset=UTF-8')
+
+        if self.callback:
+            self.set_header('Content-Type', 'application/javascript; charset=UTF-8')
+            self._write_buffer.append(utf8('%s(%s)' % (self.callback, data)))
+        else:
+            self._write_buffer.append(utf8(data))
+        self.finish()
+
+    def write_error(self, status_code, **kwargs):
+        http_explanations = {
+            400: 'Request not properly formatted or contains languages that Apertium APy does not support',
+            404: 'Resource requested does not exist. URL may have been mistyped',
+            408: 'Server did not receive a complete request within the time it was prepared to wait. Try again',
+            500: 'Unexpected condition on server. Request could not be fulfilled.',
+        }
+        explanation = kwargs.get('explanation', http_explanations.get(status_code, ''))
+        if 'exc_info' in kwargs and len(kwargs['exc_info']) > 1:
+            exception = kwargs['exc_info'][1]
+            if hasattr(exception, 'log_message') and exception.log_message:
+                explanation = exception.log_message % exception.args
+            elif hasattr(exception, 'reason'):
+                explanation = exception.reason or tornado.httputil.responses.get(status_code, 'Unknown')
+            else:
+                explanation = tornado.httputil.responses.get(status_code, 'Unknown')
+
+        result = {
+            'status': 'error',
+            'code': status_code,
+            'message': tornado.httputil.responses.get(status_code, 'Unknown'),
+            'explanation': explanation,
+        }
+
+        data = escape.json_encode(result)
+        self.set_header('Content-Type', 'application/json; charset=UTF-8')
+
+        if self.callback:
+            self.set_header('Content-Type', 'application/javascript; charset=UTF-8')
+            self._write_buffer.append(utf8('%s(%s)' % (self.callback, data)))
+        else:
+            self._write_buffer.append(utf8(data))
+        self.finish()
+
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+        self.set_header('Access-Control-Allow-Headers', 'accept, cache-control, origin, x-requested-with, x-file-name, content-type')
+
+    @tornado.web.asynchronous
+    def post(self):
+        self.get()
+
+    def options(self):
+        self.set_status(204)
+        self.finish()

apertium_apy/handlers/translate.py
@@ -1,0 +1,182 @@
+import heapq
+import logging
+import re
+import time
+from datetime import datetime
+
+from tornado import gen
+
+from apertium_apy import missing_freqs_db  # noqa: F401
+from apertium_apy.handlers.base import BaseHandler
+from apertium_apy.keys import get_key
+from apertium_apy.utils import to_alpha3_code, scale_mt_log
+from apertium_apy.utils.translation import parse_mode_file, make_pipeline
+
+
+class TranslationInfo:
+    def __init__(self, handler):
+        self.langpair = handler.get_argument('langpair')
+        self.key = handler.get_argument('key', default='null')
+        self.ip = handler.request.headers.get('X-Real-IP', handler.request.remote_ip)
+        self.referer = handler.request.headers.get('Referer', 'null')
+
+
+class TranslateHandler(BaseHandler):
+    def note_pair_usage(self, pair):
+        self.stats['useCount'][pair] = 1 + self.stats['useCount'].get(pair, 0)
+
+    unknown_mark_re = re.compile(r'[*]([^.,;:\t\* ]+)')
+
+    def maybe_strip_marks(self, mark_unknown, pair, translated):
+        self.note_unknown_tokens('%s-%s' % pair, translated)
+        if mark_unknown:
+            return translated
+        else:
+            return re.sub(self.unknown_mark_re, r'\1', translated)
+
+    def note_unknown_tokens(self, pair, text):
+        global missing_freqs_db
+        if missing_freqs_db is not None:
+            for token in re.findall(self.unknown_mark_re, text):
+                missing_freqs_db.note_unknown(token, pair)
+
+    def cleanable(self, i, pair, pipe):
+        if pipe.use_count > self.restart_pipe_after:
+            # Not affected by min_pipes_per_pair
+            logging.info('A pipe for pair %s-%s has handled %d requests, scheduling restart',
+                         pair[0], pair[1], self.restart_pipe_after)
+            return True
+        elif (i >= self.min_pipes_per_pair and
+                self.max_idle_secs != 0 and
+                time.time() - pipe.last_usage > self.max_idle_secs):
+            logging.info("A pipe for pair %s-%s hasn't been used in %d secs, scheduling shutdown",
+                         pair[0], pair[1], self.max_idle_secs)
+            return True
+        else:
+            return False
+
+    def clean_pairs(self):
+        for pair in self.pipelines:
+            pipes = self.pipelines[pair]
+            to_clean = set(p for i, p in enumerate(pipes)
+                           if self.cleanable(i, pair, p))
+            self.pipelines_holding += to_clean
+            pipes[:] = [p for p in pipes if p not in to_clean]
+            heapq.heapify(pipes)
+        # The holding area lets us restart pipes after n usages next
+        # time round, since with lots of traffic an active pipe may
+        # never reach 0 users
+        self.pipelines_holding[:] = [p for p in self.pipelines_holding
+                                     if p.users > 0]
+        if self.pipelines_holding:
+            logging.info('%d pipelines still scheduled for shutdown', len(self.pipelines_holding))
+
+    def get_pipe_cmds(self, l1, l2):
+        if (l1, l2) not in self.pipeline_cmds:
+            mode_path = self.pairs['%s-%s' % (l1, l2)]
+            self.pipeline_cmds[(l1, l2)] = parse_mode_file(mode_path)
+        return self.pipeline_cmds[(l1, l2)]
+
+    def should_start_pipe(self, l1, l2):
+        pipes = self.pipelines.get((l1, l2), [])
+        if pipes == []:
+            logging.info('%s-%s not in pipelines of this process',
+                         l1, l2)
+            return True
+        else:
+            min_p = pipes[0]
+            if len(pipes) < self.max_pipes_per_pair and min_p.users > self.max_users_per_pipe:
+                logging.info('%s-%s has ≥%d users per pipe but only %d pipes',
+                             l1, l2, min_p.users, len(pipes))
+                return True
+            else:
+                return False
+
+    def get_pipeline(self, pair):
+        (l1, l2) = pair
+        if self.should_start_pipe(l1, l2):
+            logging.info('Starting up a new pipeline for %s-%s …', l1, l2)
+            if pair not in self.pipelines:
+                self.pipelines[pair] = []
+            p = make_pipeline(self.get_pipe_cmds(l1, l2))
+            heapq.heappush(self.pipelines[pair], p)
+        return self.pipelines[pair][0]
+
+    def log_before_translation(self):
+        return datetime.now()
+
+    def log_after_translation(self, before, length):
+        after = datetime.now()
+        if self.scale_mt_logs:
+            t_info = TranslationInfo(self)
+            key = get_key(t_info.key)
+            scale_mt_log(self.get_status(), after - before, t_info, key, length)
+
+        if self.get_status() == 200:
+            oldest = self.stats['timing'][0][0] if self.stats['timing'] else datetime.now()
+            if datetime.now() - oldest > self.STAT_PERIOD_MAX_AGE:
+                self.stats['timing'].pop(0)
+            self.stats['timing'].append(
+                (before, after, length))
+
+    def get_pair_or_error(self, langpair, text_length):
+        try:
+            l1, l2 = map(to_alpha3_code, langpair.split('|'))
+        except ValueError:
+            self.send_error(400, explanation='That pair is invalid, use e.g. eng|spa')
+            self.log_after_translation(self.log_before_translation(), text_length)
+            return None
+        if '%s-%s' % (l1, l2) not in self.pairs:
+            self.send_error(400, explanation='That pair is not installed')
+            self.log_after_translation(self.log_before_translation(), text_length)
+            return None
+        else:
+            return (l1, l2)
+
+    def get_format(self):
+        dereformat = self.get_argument('format', default=None)
+        deformat = ''
+        reformat = ''
+        if dereformat:
+            deformat = 'apertium-des' + dereformat
+            reformat = 'apertium-re' + dereformat
+        else:
+            deformat = self.get_argument('deformat', default='html')
+            if 'apertium-des' not in deformat:
+                deformat = 'apertium-des' + deformat
+            reformat = self.get_argument('reformat', default='html-noent')
+            if 'apertium-re' not in reformat:
+                reformat = 'apertium-re' + reformat
+
+        return deformat, reformat
+
+    @gen.coroutine
+    def translate_and_respond(self, pair, pipeline, to_translate, mark_unknown, nosplit=False, deformat=True, reformat=True):
+        mark_unknown = mark_unknown in ['yes', 'true', '1']
+        self.note_pair_usage(pair)
+        before = self.log_before_translation()
+        translated = yield pipeline.translate(to_translate, nosplit, deformat, reformat)
+        self.log_after_translation(before, len(to_translate))
+        self.send_response({
+            'responseData': {
+                'translatedText': self.maybe_strip_marks(mark_unknown, pair, translated),
+            },
+            'responseDetails': None,
+            'responseStatus': 200,
+        })
+        self.clean_pairs()
+
+    @gen.coroutine
+    def get(self):
+        pair = self.get_pair_or_error(self.get_argument('langpair'),
+                                      len(self.get_argument('q')))
+        if pair is not None:
+            pipeline = self.get_pipeline(pair)
+            deformat, reformat = self.get_format()
+            yield self.translate_and_respond(pair,
+                                             pipeline,
+                                             self.get_argument('q'),
+                                             self.get_argument('markUnknown', default='yes'),
+                                             nosplit=False,
+                                             deformat=deformat,
+                                             reformat=reformat)

apertium_apy/handlers/__init__.py
@@ -1,0 +1,17 @@
+from apertium_apy.handlers.analyze import AnalyzeHandler  # noqa: F401
+from apertium_apy.handlers.base import BaseHandler  # noqa: F401
+from apertium_apy.handlers.coverage import CoverageHandler  # noqa: F401
+from apertium_apy.handlers.generate import GenerateHandler  # noqa: F401
+from apertium_apy.handlers.identify_lang import IdentifyLangHandler  # noqa: F401
+from apertium_apy.handlers.list_language_names import ListLanguageNamesHandler  # noqa: F401
+from apertium_apy.handlers.list_modes import ListHandler  # noqa: F401
+from apertium_apy.handlers.per_word import PerWordHandler  # noqa: F401
+from apertium_apy.handlers.pipe_debug import PipeDebugHandler  # noqa: F401
+from apertium_apy.handlers.speller import SpellerHandler  # noqa: F401
+from apertium_apy.handlers.stats import StatsHandler  # noqa: F401
+from apertium_apy.handlers.suggestion import SuggestionHandler  # noqa: F401
+from apertium_apy.handlers.translate import TranslateHandler  # noqa: F401
+from apertium_apy.handlers.translate_chain import TranslateChainHandler  # noqa: F401
+from apertium_apy.handlers.translate_doc import TranslateDocHandler  # noqa: F401
+from apertium_apy.handlers.translate_raw import TranslateRawHandler  # noqa: F401
+from apertium_apy.handlers.translate_webpage import TranslateWebpageHandler  # noqa: F401

commit d47959b1276f25897a5ce6ac17624866e87aa98f

Record unknown words in the missing-freqs DB again
The DB was reached via `from apertium_apy import missing_freqs_db`, which
binds a copy of None into each importing module, so setup_handler()'s
assignment was invisible to note_unknown_tokens() and nothing was ever
written. Broken by 34d2cda, which split the handlers out of a single
module where the global did work; flake8's F824 on the now-useless
`global` statements was silenced rather than acted on.

Keep it on BaseHandler alongside every other setting setup_handler
installs, which is also what makes it testable.

apertium_apy/__init__.py
@@ -1,12 +1,5 @@
 import random
 import string
 
-if False:
-    from typing import Optional  # noqa: F401
-
-    from apertium_apy import missingdb  # noqa: F401
-
-missing_freqs_db = None  # type: Optional[missingdb.MissingDb]  # has to be global for sig_handler :-/
-
 RECAPTCHA_VERIFICATION_URL = 'https://www.google.com/recaptcha/api/siteverify'
 BYPASS_TOKEN = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(24))

apertium_apy/apy.py
@@ -31,7 +31,7 @@ from tornado.log import enable_pretty_logging
 
 from typing import Sequence, Iterable, Type, List, Tuple, Any  # noqa: F401
 
-from apertium_apy import BYPASS_TOKEN, missing_freqs_db  # noqa: F401
+from apertium_apy import BYPASS_TOKEN
 from apertium_apy import missingdb
 from apertium_apy import systemd
 from apertium_apy.mode_search import search_path, search_prefs
@@ -64,7 +64,7 @@ from apertium_apy.handlers import (
 
 
 def sig_handler(sig, frame):
-    global missing_freqs_db  # noqa: F824
+    missing_freqs_db = BaseHandler.missing_freqs_db
     if missing_freqs_db is not None:
         if 'children' in frame.f_locals:
             for child in frame.f_locals['children']:
@@ -99,11 +99,9 @@ def setup_handler(
     memory=1000, apy_keys=None,
 ):
 
-    global missing_freqs_db
-    if missing_freqs_path:
-        missing_freqs_db = missingdb.MissingDb(missing_freqs_path, memory)
-
     handler = BaseHandler
+    if missing_freqs_path:
+        handler.missing_freqs_db = missingdb.MissingDb(missing_freqs_path, memory)
     handler.lang_names = lang_names
     handler.timeout = timeout
     handler.max_pipes_per_pair = max_pipes_per_pair

apertium_apy/handlers/base.py
@@ -14,6 +14,7 @@ from apertium_apy.utils import to_fallback_code
 # Typing imports that flake8 doesn't understand:
 from typing import Union, Dict, Optional, List, Any, Tuple  # noqa: F401
 from apertium_apy.utils.translation import FlushingPipeline, SimplePipeline  # noqa: F401
+from apertium_apy.missingdb import MissingDb  # noqa: F401
 
 
 def dump_json(data):
@@ -49,6 +50,7 @@ class BaseHandler(tornado.web.RequestHandler):
     scale_mt_logs = False
     verbosity = 0
     api_keys_conf = None
+    missing_freqs_db = None     # type: Optional[MissingDb]
     stat_period_max_age = timedelta.max
 
     # dict representing a graph of translation pairs; keys are source languages

apertium_apy/handlers/translate.py
@@ -8,7 +8,6 @@ from tornado import gen
 import tornado.iostream
 import asyncio
 
-from apertium_apy import missing_freqs_db  # noqa: F401
 from apertium_apy.handlers.base import BaseHandler
 from apertium_apy.keys import ApiKeys
 from apertium_apy.utils import to_alpha3_code, scale_mt_log
@@ -48,10 +47,9 @@ class TranslateHandler(BaseHandler):
             return re.sub(self.unknown_mark_re, r'\1', translated)
 
     def note_unknown_tokens(self, pair, text):
-        global missing_freqs_db  # noqa: F824
-        if missing_freqs_db is not None:
+        if self.missing_freqs_db is not None:
             for token in re.findall(self.unknown_mark_re, text):
-                missing_freqs_db.note_unknown(token, pair)
+                self.missing_freqs_db.note_unknown(token, pair)
 
     def cleanable(self, i, pair, pipe):
         if pipe.stuck:

tests/test_missing_freqs.py
@@ -1,0 +1,116 @@
+import os
+import sqlite3
+import tempfile
+from unittest import TestCase
+
+from apertium_apy.handlers.base import BaseHandler
+from apertium_apy.handlers.translate import TranslateHandler
+from apertium_apy.missingdb import MissingDb
+
+
+def make_handler():
+    # TranslateHandler.__init__ wants a live application and request, but
+    # note_unknown_tokens only touches class attributes, so bypass it.
+    return object.__new__(TranslateHandler)
+
+
+class TestMissingDb(TestCase):
+    def setUp(self):
+        self.db_path = os.path.join(tempfile.mkdtemp(), 'missingFreqs.db')
+
+    def rows(self):
+        with sqlite3.connect(self.db_path) as conn:
+            return sorted(conn.execute('SELECT pair, token, frequency FROM missingFreqs'))
+
+    def test_commit_writes_tokens(self):
+        db = MissingDb(self.db_path, 1000)
+        db.note_unknown('fooword', 'cat-spa')
+        db.note_unknown('barword', 'cat-spa')
+        db.commit()
+
+        self.assertEqual(self.rows(), [('cat-spa', 'barword', 1), ('cat-spa', 'fooword', 1)])
+
+    def test_repeated_tokens_accumulate(self):
+        db = MissingDb(self.db_path, 1000)
+        for _ in range(3):
+            db.note_unknown('fooword', 'cat-spa')
+        db.commit()
+
+        self.assertEqual(self.rows(), [('cat-spa', 'fooword', 3)])
+
+    def test_same_token_different_pairs_kept_apart(self):
+        db = MissingDb(self.db_path, 1000)
+        db.note_unknown('fooword', 'cat-spa')
+        db.note_unknown('fooword', 'spa-cat')
+        db.commit()
+
+        self.assertEqual(self.rows(), [('cat-spa', 'fooword', 1), ('spa-cat', 'fooword', 1)])
+
+    def test_commits_when_memory_limit_reached(self):
+        db = MissingDb(self.db_path, 1)
+        db.note_unknown('fooword', 'cat-spa')
+        db.note_unknown('barword', 'cat-spa')
+
+        # No explicit commit: exceeding the limit must have flushed to disk.
+        self.assertTrue(os.path.exists(self.db_path))
+        self.assertEqual(self.rows(), [('cat-spa', 'barword', 1), ('cat-spa', 'fooword', 1)])
+
+    def test_nothing_written_before_commit(self):
+        db = MissingDb(self.db_path, 1000)
+        db.note_unknown('fooword', 'cat-spa')
+
+        self.assertFalse(os.path.exists(self.db_path))
+
+
+class TestNoteUnknownTokens(TestCase):
+    def setUp(self):
+        self.db_path = os.path.join(tempfile.mkdtemp(), 'missingFreqs.db')
+        # setup_handler() installs the db here; commit on every token so the
+        # assertions can read it straight back off disk.
+        BaseHandler.missing_freqs_db = MissingDb(self.db_path, 0)
+
+    def tearDown(self):
+        BaseHandler.missing_freqs_db = None
+
+    def rows(self):
+        with sqlite3.connect(self.db_path) as conn:
+            return sorted(conn.execute('SELECT pair, token, frequency FROM missingFreqs'))
+
+    def test_marked_tokens_are_recorded(self):
+        # Regression test: the db used to be a module-level global reached via
+        # `from apertium_apy import missing_freqs_db`, which bound a copy of
+        # None into each importing module. setup_handler()'s assignment was
+        # therefore invisible here and no unknown word was ever recorded.
+        make_handler().note_unknown_tokens('cat-spa', 'Hola *fooword i *barword')
+        BaseHandler.missing_freqs_db.commit()
+
+        self.assertEqual(self.rows(), [('cat-spa', 'barword', 1), ('cat-spa', 'fooword', 1)])
+
+    def test_repeated_tokens_accumulate(self):
+        make_handler().note_unknown_tokens('cat-spa', '*fooword i *fooword')
+        BaseHandler.missing_freqs_db.commit()
+
+        self.assertEqual(self.rows(), [('cat-spa', 'fooword', 2)])
+
+    def test_unmarked_text_records_nothing(self):
+        make_handler().note_unknown_tokens('cat-spa', 'Hola com estas')
+        BaseHandler.missing_freqs_db.commit()
+
+        self.assertEqual(self.rows(), [])
+
+    def test_recorded_even_when_marks_are_stripped(self):
+        # markUnknown=no strips the asterisks from the response, but the words
+        # are still unknown and must be counted.
+        handler = make_handler()
+        translated = handler.maybe_strip_marks(False, ('cat', 'spa'), 'Hola *fooword')
+        BaseHandler.missing_freqs_db.commit()
+
+        self.assertEqual(translated, 'Hola fooword')
+        self.assertEqual(self.rows(), [('cat-spa', 'fooword', 1)])
+
+    def test_no_db_configured_is_a_noop(self):
+        BaseHandler.missing_freqs_db = None
+
+        make_handler().note_unknown_tokens('cat-spa', 'Hola *fooword')
+
+        self.assertFalse(os.path.exists(self.db_path))